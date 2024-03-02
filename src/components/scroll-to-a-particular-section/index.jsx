a9e902f1013ef0f59edb5aad63be696e72a07546
anyars-encarta <anyarsencarta@gmail.com>
2024-03-02 09:24:54 +0000

    Setup Scroll Component

---
+++ src/components/scroll-to-a-particular-section/index.jsx
@@ -0,0 +1,13 @@
+const ScrollToAParticularSection = () => {
+    return (
+        <div>
+            <section>First Card</section>
+            <section>Second Card</section>
+            <section>Third Card</section>
+            <section>Fourth Card</section>
+            <section>Fifth Card</section>
+        </div>
+    );
+};
+
+export default ScrollToAParticularSection;
