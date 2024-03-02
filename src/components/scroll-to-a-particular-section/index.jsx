a4c14c782e4879630f424bb032de0788194b8977
anyars-encarta <anyarsencarta@gmail.com>
2024-03-02 14:03:51 +0000

    Correct eslint errors

--- src/components/scroll-to-a-particular-section/index.jsx
+++ src/components/scroll-to-a-particular-section/index.jsx
@@ -2,75 +2,82 @@ import { useRef } from 'react';
 import './styles.css';
 
 const ScrollToAParticularSection = () => {
-    const ref = useRef();
+  const ref = useRef();
 
-    const data = [
-        {
-            label: 'First Card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'red'
-            }
-        },
-        {
-            label: 'Second Card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'grey'
-            }
-        },
-        {
-            label: 'Third Card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'blue'
-            }
-        },
-        {
-            label: 'Fourth Card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'green'
-            }
-        },
-        {
-            label: 'Fifth Card',
-            style: {
-                width: '100%',
-                height: '600px',
-                background: 'goldenrod'
-            }
-        }
-    ];
+  const data = [
+    {
+      label: 'First Card',
+      style: {
+        width: '100%',
+        height: '600px',
+        background: 'red',
+      },
+    },
+    {
+      label: 'Second Card',
+      style: {
+        width: '100%',
+        height: '600px',
+        background: 'grey',
+      },
+    },
+    {
+      label: 'Third Card',
+      style: {
+        width: '100%',
+        height: '600px',
+        background: 'blue',
+      },
+    },
+    {
+      label: 'Fourth Card',
+      style: {
+        width: '100%',
+        height: '600px',
+        background: 'green',
+      },
+    },
+    {
+      label: 'Fifth Card',
+      style: {
+        width: '100%',
+        height: '600px',
+        background: 'goldenrod',
+      },
+    },
+  ];
 
-    const handleGoToSection = () => {
-        // ref.current.scrollIntoView({ behavior: 'smooth' });  // this code works fine
+  const handleGoToSection = () => {
+    // ref.current.scrollIntoView({ behavior: 'smooth' });  // this code works fine
 
-        let pos = ref.current.getBoundingClientRect().top;
+    const pos = ref.current.getBoundingClientRect().top;
 
-        window.scrollTo({
-            top: pos,
-            behavior: 'smooth',
-        });
-    };
+    window.scrollTo({
+      top: pos,
+      behavior: 'smooth',
+    });
+  };
 
-    return (
-        <div>
-            <h1>Scroll to a particular section</h1>
-            <button onClick={handleGoToSection} type="button">Click To Scroll</button>
+  return (
+    <div>
+      <h1>Scroll to a particular section</h1>
+      <button onClick={handleGoToSection} type="button">Click To Scroll</button>
 
-            {
-                    data.map((dataItem, index) =>
-                        <div ref={index === 4 ? ref : null} style={ dataItem.style }>
-                            <h3>{dataItem.label}</h3>
-                        </div>)
-            }
-        </div>
-    );
+      {
+        data.map((dataItem, index) => (
+          <div
+            ref={index === 4
+              ? ref
+              : null}
+            style={dataItem.style}
+            key={dataItem.label}
+          >
+            <h3>{dataItem.label}</h3>
+          </div>
+        ))
+      }
+    </div>
+  );
 };
 
 export default ScrollToAParticularSection;
--- src/components/scroll-to-a-particular-section/multiple-sections.jsx
+++ src/components/scroll-to-a-particular-section/multiple-sections.jsx
@@ -2,69 +2,69 @@ import { useRef } from 'react';
 import './styles.css';
 
 const ScrollToAParticularSectionMultiple = () => {
-    const section1 = useRef(null);
-    const section2 = useRef(null);
-    const section3 = useRef(null);
-    const section4 = useRef(null);
-    const section5 = useRef(null);
+  const section1 = useRef(null);
+  const section2 = useRef(null);
+  const section3 = useRef(null);
+  const section4 = useRef(null);
+  const section5 = useRef(null);
 
-    const handleGoToSection1 = () => {
-        section1.current.scrollIntoView({ behavior: 'smooth' });
-    };
+  const handleGoToSection1 = () => {
+    section1.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    const handleGoToSection2 = () => {
-        section2.current.scrollIntoView({ behavior: 'smooth' });
-    };
+  const handleGoToSection2 = () => {
+    section2.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    const handleGoToSection3 = () => {
-        section3.current.scrollIntoView({ behavior: 'smooth' });
-    };
+  const handleGoToSection3 = () => {
+    section3.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    const handleGoToSection4 = () => {
-        section4.current.scrollIntoView({ behavior: 'smooth' });
-    };
+  const handleGoToSection4 = () => {
+    section4.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    const handleGoToSection5 = () => {
-        section5.current.scrollIntoView({ behavior: 'smooth' });
-    };
+  const handleGoToSection5 = () => {
+    section5.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
-    return (
-        <div className="content">
-            <div className="nav">
-                <h1>Scroll to a particular section</h1>
+  return (
+    <div className="content">
+      <div className="nav">
+        <h1>Scroll to a particular section</h1>
 
-                <div className="scroll-buttons">
-                    <button onClick={handleGoToSection1} type="button">Go To First Card</button>
-                    <button onClick={handleGoToSection2} type="button">Go To Second Card</button>
-                    <button onClick={handleGoToSection3} type="button">Go To Third Card</button>
-                    <button onClick={handleGoToSection4} type="button">Go To Fourth Card</button>
-                    <button onClick={handleGoToSection5} type="button">Go To Fifth Card</button>
-                </div>
-            </div>
-
-            <div className="cards">
-                <section ref={section1} className="section-1">
-                    <h3>First Card</h3>
-                </section>
-
-                <section ref={section2} className="section-2">
-                    <h3>Second Card</h3>
-                </section>
-
-                <section ref={section3} className="section-3">
-                    <h3>Third Card</h3>
-                </section>
-
-                <section ref={section4} className="section-4">
-                    <h3>Fourth Card</h3>
-                </section>
-
-                <section ref={section5} className="section-5">
-                    <h3>Fifth Card</h3>
-                </section>
-            </div>
+        <div className="scroll-buttons">
+          <button onClick={handleGoToSection1} type="button">Go To First Card</button>
+          <button onClick={handleGoToSection2} type="button">Go To Second Card</button>
+          <button onClick={handleGoToSection3} type="button">Go To Third Card</button>
+          <button onClick={handleGoToSection4} type="button">Go To Fourth Card</button>
+          <button onClick={handleGoToSection5} type="button">Go To Fifth Card</button>
         </div>
-    );
+      </div>
+
+      <div className="cards">
+        <section ref={section1} className="section-1">
+          <h3>First Card</h3>
+        </section>
+
+        <section ref={section2} className="section-2">
+          <h3>Second Card</h3>
+        </section>
+
+        <section ref={section3} className="section-3">
+          <h3>Third Card</h3>
+        </section>
+
+        <section ref={section4} className="section-4">
+          <h3>Fourth Card</h3>
+        </section>
+
+        <section ref={section5} className="section-5">
+          <h3>Fifth Card</h3>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default ScrollToAParticularSectionMultiple;
